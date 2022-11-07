3956d24dc5d6aa5fd45d78c4ac61209dd7affc3d
Reynaldo <pr19004@ues.edu.sv>
2022-11-06 20:54:00 -0600

    hosting 2 sprint 2

--- Config/Config.php
+++ Config/Config.php
@@ -3,10 +3,10 @@
 //Variables constantes acceder a directorios
 
 
-const BASE_URL = "http://localhost/tienda_virtual";
+//const BASE_URL = "http://localhost/tienda_virtual";
 
 //url en la nube
-//const BASE_URL = "https://tienda-virtual-pr19004.herokuapp.com";
+const BASE_URL = "https://tienda-virtual-pr19004.herokuapp.com";
 
 //Zona horaria
 date_default_timezone_set('America/El_Salvador');
@@ -14,23 +14,23 @@ date_default_timezone_set('America/El_Salvador');
 
 const LIBS = "Libraries/";
 const VIEWS = "Views/";
-
+/*
 //Datos para la conexion a base de datos
 const DB_HOST = "localhost";
 const DB_NAME = "db_tiendavirtual";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_CHARSET = "charset=utf8";
+*/
 
 
-/*
 //Datos para la conexion a base de datos externa
 
 const DB_HOST = "us-cdbr-east-06.cleardb.net";
 const DB_NAME = "heroku_79fb5a24eba3054";
 const DB_USER = "b851d76bdd4c24";
 const DB_PASSWORD = "da646ecc";
-const DB_CHARSET = "charset=utf8";*/
+const DB_CHARSET = "charset=utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
 	const SPD = ".";
